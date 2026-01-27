c4eceae04487fce3827b5ef075b09448bee74e03
SalahHassan202 <salahhassan66190@gmail.com>
2026-01-27 22:45:56 +0200

    update 3 files

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,76 @@
+import 'package:first_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/onboarding.png"),
+
+              const SizedBox(height: 20),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    style: GoogleFonts.spaceGrotesk(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    children: const [
+                      TextSpan(text: "Discover, Collect and Sell "),
+                      TextSpan(
+                        text: "NFTs",
+                        style: TextStyle(color: Color(0xFFA3D80D)),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 8),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Text(
+                  "A vibrant marketplace where creators showcase their unique NFTs.",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.spaceGrotesk(fontSize: 16),
+                ),
+              ),
+
+              const SizedBox(height: 60),
+
+              Padding(
+                padding: const EdgeInsets.all(32),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset("assets/Slider.png"),
+                    GestureDetector(
+                      onTap: () => Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (_) => const HomeScreen()),
+                      ),
+                      child: Image.asset("assets/Button.png"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
